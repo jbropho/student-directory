b8e54347a4df279267ba77de4f0d995aab655ab9
Jordan Brooks <jorbrooks92@gmail.com>
2018-03-13 11:22:34 +0000

    refactor interactive menu method into multiple methods

--- directory.rb
+++ directory.rb
@@ -1,44 +1,51 @@
 require 'date'
 
-def interactive_menu
-  students = []
-  loop do
-    # 1. print the menu and ask the user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more items
-    # 2. read the input and save it into a variable
-    selection = gets.chomp
-    # 3. do what the user has asked
-    case selection
-    when "1"
-      students = input_students
-    when "2"
-      print_header
-      print(students)
-      print_footer(students)
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
-    end
-  end
-end
+@students = []
 
 def input_students
-  students = []
   month = get_month
   puts "Please enter a student name"
   puts "Hit enter twice to quit"
   name = gets.chomp
   
   while !name.empty? do
-    puts "Now we have #{students.count + 1} students"
+    puts "Now we have #{@students.count + 1} students"
     month = get_month if change_month?
-    students << { name: name, cohort: month }
+    @students << { name: name, cohort: month }
     name = gets.chomp
   end
-  students
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+  when "1"
+    input_students
+  when "2"
+    show_students
+  when "9"
+    exit # this will cause the program to terminate
+  else
+    puts "I don't know what you meant, try again"
+  end
 end
 
 def print_header
@@ -63,8 +70,8 @@ def get_month
 end 
 
 # prints all students, accepts an optional block to filter results
-def print(students, condition = -> (student) { true } )
-  students.each_with_index do |student, index|
+def print_student_list(condition = -> (student) { true } )
+  @students.each_with_index do |student, index|
     if condition.call(student)
       puts " #{index + 1} : #{student[:name]} (#{student[:cohort]} cohort)"
       .center(50)
@@ -86,17 +93,17 @@ def print_month(sorted_by_month)
   end
 end
 
-def sort_by_month(students)
+def sort_by_month
   by_month = Hash.new
-  students.each do |student|
+  @students.each do |student|
     by_month[student[:cohort]] ||= Array.new
     by_month[student[:cohort]] << student
   end 
   by_month
 end 
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students".center(50)
+def print_footer
+  puts "Overall, we have #{@students.count} great students".center(50)
 end
 
 
