0d78236f917320f7080566c22d9b61d1670d9bd0
Jordan Brooks <jorbrooks92@gmail.com>
2018-03-13 11:14:26 +0000

    add interactive menu

--- directory.rb
+++ directory.rb
@@ -1,5 +1,30 @@
 require 'date'
 
+def interactive_menu
+  students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+  end
+end
+
 def input_students
   students = []
   month = get_month
@@ -74,16 +99,13 @@ def print_footer(students)
   puts "Overall, we have #{students.count} great students".center(50)
 end
 
-students = input_students
-print_header
 
 # example conditions
 length = -> (student) { student[:name].size > 12 }
 character = -> (student) { student[:name].start_with?('a') }
 # pass condition as second argument to print
-print(students)
-puts "-" * 50
-print_footer(students)
-puts "-" * 50 
-sorted = sort_by_month(students)
-print_month(sorted)
+# print(students)
+
+# sorted = sort_by_month(students)
+# print_month(sorted)
+interactive_menu
