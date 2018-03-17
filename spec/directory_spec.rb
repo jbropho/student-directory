2db7d727c64148af81cf1f027bd4965ccc1075ed
Jordan Brooks <jorbrooks92@gmail.com>
2018-03-17 11:36:31 +0000

    test print_all_students and print_by_month methods

--- spec/directory_spec.rb
+++ spec/directory_spec.rb
@@ -3,7 +3,6 @@ require 'csv'
 require 'yaml'
 
 describe "Print students" do
- 
   before :all do
       students = [
         { name: "Dr. Hannibal Lecter", cohort: :november},
@@ -19,4 +18,22 @@ describe "Print students" do
   before :each do
     @students =  YAML::load(File.read("students.yml"))
   end
+
+  it 'prints all students' do 
+    expect(print_student_list).to eq(
+      [{:name=>"Dr. Hannibal Lecter", :cohort=>:november},
+       {:name=>"Darth Vader", :cohort=>:january},
+       {:name=>"Alex DeLarge", :cohort=>:march},
+       {:name=>"The Wicked Witch of the West", :cohort=>:november}])
+  end 
+
+  it 'prints by month' do 
+    sorted = sort_by_month
+    expect(print_by_month(sorted)).to eq(
+       :january => [{:name=>"Darth Vader", :cohort=>:january}],
+       :march => [{:name=>"Alex DeLarge", :cohort=>:march}],
+       :november => [{:name=>"Dr. Hannibal Lecter", :cohort=>:november},
+       {:name=>"The Wicked Witch of the West", :cohort=>:november}]  
+    )
+  end 
 end 
